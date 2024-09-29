fdc8d2a77739ea217d1ca88fd4e75a7aafdda20c
jay-1115 <152771692+jay-1115@users.noreply.github.com>
2024-09-30 09:43:10 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/JS-PlantShoppingApp-React",
+//  base: "/JS-PlantShoppingApp-React",
+  base: "https://jay-1115.github.io/JS-PlantShoppingApp-React/",
   plugins: [react()],
 })
